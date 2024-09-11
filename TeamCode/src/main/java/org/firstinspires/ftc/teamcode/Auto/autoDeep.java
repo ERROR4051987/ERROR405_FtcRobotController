55caba8d2a4040cfef3e86c4a4d1c24325697327
carterb <CarterB_821@protonmail.com>
2024-09-11 12:40:37 -0500

    WIP - autoDeep

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/autoDeep.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous (name= "autoDeep", group= "intoTheDeep")
+public class autoDeep extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+    }
+}
